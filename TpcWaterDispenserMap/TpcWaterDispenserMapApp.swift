bdfb92fec19b16cd3debfdbe5b89a1f883458155
Riddle Ling <riddle.apple@gmail.com>
2023-01-10 17:33:23 +0800

    Initial Commit

---
+++ TpcWaterDispenserMap/TpcWaterDispenserMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TpcWaterDispenserMapApp.swift
+//  TpcWaterDispenserMap
+//
+//  Created by Riddle Ling on 2023/1/10.
+//
+
+import SwiftUI
+
+@main
+struct TpcWaterDispenserMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
